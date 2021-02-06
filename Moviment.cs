cfdbdfa868b6dc425447475f452998d1aefa439e
Sasy-Merko <78384995+Sasy-Merko@users.noreply.github.com>
2021-02-06 17:24:11 +0100

    Add files via upload

---
+++ Moviment.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moviment : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //se muovo il tasto, muovi l'oggetto sull'asse corrispondente al tasto.
+        // Horizontal
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                Debug.Log("Stai andando verso Destra");
+            }
+            else
+            {
+                Debug.Log("Stai andando verso Sinistra");
+            }
+        }
+    }
+}
