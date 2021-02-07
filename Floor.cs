55e8f861cb683252c0b62f9ae4b2710320919d56
Sasy-Merko <78384995+Sasy-Merko@users.noreply.github.com>
2021-02-07 20:20:13 +0100

    Add files via upload

---
+++ Floor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+
+    //questa funzione viene richiamata quando un collider entra o attraversa un collider trigger.
+
+     void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("E' entrato " + other.gameObject.name);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("E' uscito " + other.gameObject.name);
+    }
+}
