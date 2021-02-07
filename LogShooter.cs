5840a46f3c6a8d029f273d363e2ef830e6d80ba8
Sasy-Merko <78384995+Sasy-Merko@users.noreply.github.com>
2021-02-07 20:54:45 +0100

    Add files via upload
    Questi vengono utilizzati per il GetComponent per far interagire 2 script su uno stesso oggetto o oggetti differenti.

---
+++ LogShooter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogShooter : MonoBehaviour {
+
+    public int score;
+
+
+    //andrà a stampare il messaggio che gli passiamo come parametro.
+	public void ShootLog(string message)
+    {
+        Debug.Log("Shooting: " + message);
+    }
+}
---
+++ ShooterTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterTrigger : MonoBehaviour
+{
+
+    public GameObject logShooterObject;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //quando viene premuto il tasto S della tastiera
+        //getkeydown è l'evento quando un tasto viene premuto.
+        //quando lo rilascio è getkeyup
+        //solo getkey e quando continua ad essere premuto.
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            LogShooter logShooter = logShooterObject.GetComponent<LogShooter>();
+            //se esiste il logshooter spara questa cosa qua altrimenti no 
+            //questo se nel nostro oggetto non teniamo il logshooter script.
+            if (logShooter)
+            {
+                logShooter.score = 10;
+                logShooter.ShootLog("Hello");
+            }
+        }
+    }
+}
