6bca1e165695441529d838991dffe81d923b8bb5
Sasy-Merko <78384995+Sasy-Merko@users.noreply.github.com>
2021-02-07 20:20:50 +0100

    Add files via upload

---
+++ Sphere.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        //possiamo anche mettere collision.collider è la stessa cosa.
+        Debug.Log("Ho colliso con " + collision.gameObject.name); 
+    }
+
+    
+
+}
