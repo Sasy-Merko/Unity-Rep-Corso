28d86356bee7bcfbd8b3fae038dc23afb81ad59a
Sasy-Merko <78384995+Sasy-Merko@users.noreply.github.com>
2021-02-21 14:06:50 +0100

    Add files via upload
    Script per quanto riguarda gli eventi in game come energia etc.

---
+++ BluePill.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BluePill : Pill
+{
+    public int healAmount = 10;
+
+    protected override void OnPicked(Collider other)
+    {
+        base.OnPicked(other);
+        HealtManager healtManager = other.GetComponent<HealtManager>();
+        if (!healtManager) { return; }
+
+        healtManager.Heal(healAmount);
+        Destroy(gameObject);
+    }
+}
---
+++ HealtManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealtManager : MonoBehaviour
+{
+    public int healt;
+    public int maxhealt;
+
+    void Start()
+    {
+        healt = maxhealt;
+    }
+
+    public void Heal(int amount) 
+    {
+        Damage(-amount);
+    }
+    public void Damage(int damageTaken)
+    {
+        healt -= damageTaken;
+        if (healt < 1)
+        {
+            Debug.LogError("You Dead");
+            GetComponent<UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController>().enabled = false;
+        
+        }
+
+        if (healt > maxhealt)
+        {
+            healt = maxhealt;
+        }
+
+        Debug.Log("Adesso hai " + healt + " punti vita");
+    }
+}
